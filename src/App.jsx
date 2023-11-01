fe4c4fdc6cb1db40a69a13e7c68cbad0dc5501ed
rkrakibhasan680 <rkrakibhasan680@gmail.com>
2023-11-01 18:29:40 +0600

    responsive check

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Gallery from "./components/Gallery";
 function App() {
 
   return (
-    <section className="w-full min-h-screen flex justify-center items-center ">
+    <section className="w-full min-h-screen flex justify-center items-center sm:mb-10 mb-0">
      <Gallery/>
     </section>
   )

c814ff155514c52f0e4284f8908b6b14d4efc74f
rkrakibhasan680 <rkrakibhasan680@gmail.com>
2023-11-02 14:46:00 +0600

    update hover and drag drop

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 
 
 import Gallery from "./components/Gallery";
+
 function App() {
 
   return (
     <section className="w-full min-h-screen flex justify-center items-center sm:mb-10 mb-0">
      <Gallery/>
+ 
     </section>
   )
 }
--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,7 +1,6 @@
 
 import { useState } from 'react';
 import { Images } from '../data/ImageData';
-import Loading from './Loading/Loading';
 const Gallery = () => {
     // after select a img it store there 
     const [selectedImages, setSelectedImages] = useState([]);
@@ -20,7 +19,7 @@ const Gallery = () => {
 
     };
 
-  
+
     const handleDragStart = (e, image) => {
         e.dataTransfer.setData('imageId', image._id);
         setDraggedImage(image);
@@ -28,6 +27,7 @@ const Gallery = () => {
 
     const handleDragOver = (e) => {
         e.preventDefault();
+        console.log("ki asbe ")
     };
 
     const handleDrop = (e, targetImage) => {
@@ -50,68 +50,75 @@ const Gallery = () => {
         setImages(deleteItemFilter);
         setSelectedImages([])
     }
- if(Images.length===0){
-    return <Loading/>
- }
 
 
-    return (
-        <div className="gallery-main pb-5 px-2">
-            {/* gallery top */}
 
-            {selectedImages.length === 0 ? <p className='text-xl font-bold p-5'>Gallery</p>
-                :
-                <div className='flex justify-between p-5'>
-                    <div className='flex justify-center items-center'>
-                        <input className='w-4 h-4 ' type="checkbox" checked />
-                        <p className='pl-2 text-xl font-bold'>{selectedImages.length} FIles Selected</p>
-                    </div >
-                    <div className='pr-23'>
-                        <button
-                            onClick={handleDelete}
-                            className='text-[red]'>Delete File</button></div>
-                </div>
-            }
+    return (<div className="gallery-main pb-5 px-2">
+        {/* gallery top */}
 
-
-            <hr className='border-0 h-2 bg-gray-700 mb-2' />
-
-
-            {/* gallery body  */}
-
-            <div className="gallery-body">
-
-                {images.map((image) => (
-                    <div
-                        className="gallery-item bg-black"
-                        key={image._id}
-                        onDragOver={handleDragOver}
-                        onDrop={(e) => handleDrop(e, image)}
-                    >
-                        <img
-                            onClick={() => handleImageClick(image._id)}
-                            src={image.image}
-                            className={` hover:opacity-[0.5]   duration-300 bg-white`}
-                            alt={`Image ${image._id}`}
-                            draggable="true"
-                            onDragStart={(e) => handleDragStart(e, image)}
-                        />
-                        {selectedImages.includes(image._id) && (
-                            <div
-                                onClick={() => handleImageClick(image._id)}
-                                className={` image-checkbox w-full h-full bg-black opacity-[0.2]`}>
-                                <input className="w-5 h-5" type="checkbox" checked />
-                            </div>
-                        )}
-                    </div>
-                ))}
-                <div className="flex flex-col justify-center items-center  min-w-[180px] min-h-[180px]" >
-                    <img style={{ width: "30px", height: "30px" }} src="/icon/gallery.png" alt="Icon" />
-                    <p>Add Image</p>
-                </div>
-
+        {selectedImages.length === 0 ? <p className='text-xl font-bold p-5 flex items-center '>Gallery</p>
+            :
+            <div className='flex justify-between items-center p-5 '>
+                <div className='flex justify-center items-center'>
+                    <input className='w-4 h-4 ' type="checkbox" checked />
+                    <p className='pl-2 text-xl font-bold'>{selectedImages.length} FIles Selected</p>
+                </div >
+                <div className='pr-23'>
+                    <button
+                        onClick={handleDelete}
+                        className='text-[red]'>Delete File</button></div>
             </div>
+        }
+
+
+        <hr className='border-0 h-2 bg-gray-700 mb-3' />
+
+
+        {/* gallery body  */}
+
+        <div className="gallery-body ">
+
+            {images?.map((image) => (
+                <div
+                    className="gallery-item relative"
+                    key={image._id}
+                    onDragOver={handleDragOver}
+                    onDrop={(e) => handleDrop(e, image)}
+                >
+                    <div
+                        className='w-full h-full'
+                        draggable="true"
+                        onDragStart={(e) => handleDragStart(e, image)}>
+
+                        <img
+                            src={image.image}
+                            className={` hover:opacity-[0.5] w-full h-full rounded duration-300 bg-white`}
+                            alt={`Image ${image._id}`}
+
+                        />
+
+                        <div
+
+                            className='absolute w-full  h-full bg-black top-0 left-0 opacity-0  hover:opacity-50 duration-300 cursor-move'>
+                            <input checked={false} onClick={() => handleImageClick(image._id)} className='h-5 w-5 ml-5 mt-5 cursor-pointer' type="checkbox" name="checkbox" id="checkbox" />
+                        </div>
+                    </div>
+                    {selectedImages.includes(image._id) && (
+                        <div
+                            className={` absolute w-full h-full bg-black top-0 left-0 opacity-30 cursor-auto`}>
+                            <input checked onClick={() => handleImageClick(image._id)} className='h-5 w-5 ml-5 mt-5 cursor-pointer' type="checkbox" name="checkbox2" id="checkbox2" />
+                        </div>
+                    )}
+                </div>
+            ))}
+            <div className="flex flex-col justify-center items-center  min-w-[180px] min-h-[180px]" >
+                <img style={{ width: "30px", height: "30px" }} src="/icon/gallery.png" alt="Icon" />
+                <p>Add Image</p>
+            </div>
+
         </div>
+    </div>
+
     );
 };
 
